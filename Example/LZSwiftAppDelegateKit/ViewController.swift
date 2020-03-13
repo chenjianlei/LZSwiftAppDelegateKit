7778cab6f4ee7cfe1caaa5bd3d86cf17b762e35d
chenjianlei <woshixiaolei@qq.com>
2020-03-13 19:13:19 +0800

    test

--- Example/LZSwiftAppDelegateKit/ViewController.swift
+++ Example/LZSwiftAppDelegateKit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("test")
+        print("test1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
